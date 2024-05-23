d2d117b47fe9bb4cf9d57f1140e8866489ed984b
janghyunroh <janghyunroh@gmail.com>
2024-05-23 22:02:36 +0900

    added IUPC2024 and gitignore file

---
+++ IUPC2024/US_E_31849.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+vector<pair<int, pair<int, int>>> rooms;
+bool convs[1001][1001] = {false, };
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+int N, M, R, C;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> N >> M >> R >> C;  
+    for(int i = 0; i < R; i++) {
+        int a, b, p; cin >> a >> b >> p;
+        pair<int, int> room = make_pair(a, b);
+        rooms.emplace_back(p, room);
+    }
+    
+    for(int i = 0; i < C; i++) {
+        int c, d; cin >> c >> d;
+        convs[c][d] = true;
+    }
+    
+    //BFS
+    priority_queue<pair<int, pair<int, pair<int, int>>>> pq;
+    
+    for(auto room : rooms) {
+        pq.emplace(0, room);
+    }
+    
+    //BFS 수행
+    while(pq.size()) {
+        auto it = pq.top(); pq.pop();
+
+        int dist = -it.first;
+        int p = it.second.first;
+        int convx = it.second.second.first;
+        int convy = it.second.second.second;
+        
+        //편의점을 찾은 경우
+        if(convs[convx][convy]) {
+            cout << dist;
+            return 0;
+        }
+        
+        //
+        for(int i = 0; i < 4; i++) {
+            int newx = convx + dx[i];
+            int newy = convy + dy[i];
+            if(newx > 0 && newx <= N && newy > 0 && newy <= M) {
+                pair<int, int> next = make_pair(newx, newy);
+                pair<int, pair<int, int>> next_room = make_pair(p, next);
+                pq.emplace(-(dist + p), next_room);
+            }
+                
+        }        
+    }
+    
+    
+}
